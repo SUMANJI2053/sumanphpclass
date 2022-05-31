967bf7fa24825792630c8b9ee7bf79ac137fc97d
sumansingh346 <104128719+sumansingh346@users.noreply.github.com>
2022-05-31 14:16:07 +0545

    2d model example process diagram to flowchart

---
+++ model.php
@@ -0,0 +1,22 @@
+<?php
+model:
+$length = (int)readline('Input Length : ');
+$width = (int)readline('Input Width : ');
+if ($length <=0 || $width <= 0)
+{
+    goto model;
+}
+$color = readline('Input Color : ');
+$opacity = (float)readline('Input Opacity : ');
+echo 'Our model has color : '.$color.' and opacity : '.$opacity.PHP_EOL;
+
+$color = readline('Input Cornor : ');
+$opacity = (int)readline('Input softness : ');
+echo 'Our model has color : '.$color.' and opacity : '.$opacity;
+
+$color = readline('Input border : ');
+$opacity = (int)readline('Input border type : ');
+echo 'Our model has color : '.$color.' and opacity : '.$opacity;
+
+echo 'all variable';
+?>
