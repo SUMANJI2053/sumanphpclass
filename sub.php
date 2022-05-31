1d304a55b72b20c697d3d7c997ecb654a0db5284
suman singh <sumanmahato2078@gmaim.com>
2022-05-31 13:34:40 +0545

    example

---
+++ sub.php
@@ -0,0 +1,7 @@
+<?php
+$x = readline('Input X :');
+$y = readline('Input Y :');
+$z = readline('Input z :');
+$sub = $x-$y-$z;
+echo 'sub of X-Y-z is :'. $sub;
+?>
---
+++ sum.php
@@ -0,0 +1,8 @@
+<?php
+$x = readline('Input  X :');
+$y = readline('Input  Y :');
+$z = readline('Input  z :');
+$A = readline('Input  A :');
+$multiply = $x*$y*$z*$A;
+echo 'multiply of X*Y*z*A :'. $multiply;
+?>
