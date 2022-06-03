dc3d3396924219fd9d07a8a0f98e07eae9e1e254
sumanji2053 <sumansingh205327@gmail.com>
2022-06-03 01:44:01 -0700

    Update sum.php

--- sum.php
+++ sum.php
@@ -4,5 +4,5 @@ $y = readline('Input  Y :');
 $z = readline('Input  z :');
 $A = readline('Input  A :');
 $multiply = $x*$y*$z*$A;
-echo 'multiply of X*Y*z*A :'. $multiply;
+echo 'multiply  X*Y*z*A :'. $multiply;
 ?>
